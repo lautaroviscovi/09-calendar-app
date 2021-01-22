3bdb26272cdb748b0cfbb5d52a0c3fd1ce567d2d
lautaroviscovi <lautaroviscovi@gmail.com>
2021-01-22 02:52:26 -0300

    bootstrap&router-add

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from './App';
+import { CalendarApp } from './CalendarApp';
 
 ReactDOM.render(
-    <App />,
+    <CalendarApp />,
   document.getElementById('root')
 );
---
+++ src/router/AppRouter.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Redirect,
+    Route, 
+  } from "react-router-dom";
+
+import { LoginScreen } from '../components/auth/LoginScreen';
+import { CalendarScreen } from '../components/calendar/CalendarScreen';
+
+export const AppRouter = () => {
+    return (
+            <Router>
+                <div>
+                    <Switch>
+                        
+                        <Route path = '/login'
+                        component = { LoginScreen }
+                        />
+                    
+
+                        <Route
+                        exact
+                        path = '/'
+                        component = { CalendarScreen }
+                        />
+
+                        <Redirect to = '/' />
+
+                    </Switch>
+                </div>
+            
+            </Router>
+    )
+};
