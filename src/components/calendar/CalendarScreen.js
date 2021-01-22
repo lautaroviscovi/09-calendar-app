cfada1116e0ce9390514c419108f88e0675e1f95
lautaroviscovi <lautaroviscovi@gmail.com>
2021-01-22 03:58:27 -0300

    calendar-events-add

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,29 +1,79 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
+
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 import { NavBar } from '../ui/NavBar';
+import { CalendarEvent } from './CalendarEvent';
 
 const localizer = momentLocalizer( moment );
 
 const events = [ {
-    title: 'hola',
+    title: 'Test event',
     start: moment().toDate(),
     end: moment().add( 2, 'hours' ).toDate(),
     bgcolor: '#fafafa',
+    notes: 'add',
+    user: {
+        _id: '123',
+        name: 'Elon'
+    }
 } ]
 
 export const CalendarScreen = () => {
+
+    const [ lastView, setlastView ] = useState( localStorage.getItem( 'lastView' ) || 'month' );
+
+    // Trae los datos de la agenda
+    const onDoubleClick = ( e ) => {
+        console.log(e)
+    }
+    // Selecciona el evento
+    const onSelectEvent = ( e ) => {
+        console.log(e)
+    }
+    // Muestra la vista (dia, semana, mes, año)
+    // El localStorage mantiene el estado
+    // Si estoy en semana y recargo, debe continuar
+    // en semana y no cambiar a dia, por ejemplo
+    const onViewChange = ( e ) => {
+        setlastView( e );
+        localStorage.setItem( 'lastView', e );
+    }
+    // Pack de estilos para el evento
+    const eventStyleGetter = ( event, start, end, isSelected ) => {
+
+        const style = {
+            backgroundColor: '#367CF7',
+            borderRadius: '0px',
+            opacity: 0.8,
+            display: 'block',
+            color: 'white',
+        }
+
+        return {
+            style
+        }
+    };
+
     return (
         <div className = 'calendar-screen'>
             <NavBar />
 
             <Calendar
-                localizer = {localizer}
-                events = {events}
-                startAccessor = "start"
-                endAccessor = "end"
+                localizer = { localizer }
+                events = { events }
+                startAccessor = 'start'
+                endAccessor = 'end'
+                eventPropGetter = { eventStyleGetter }
+                onDoubleClickEvent = { onDoubleClick }
+                onSelectEvent = { onSelectEvent }
+                onView = { onViewChange }
+                view = { lastView }
+                components = { {
+                    event: CalendarEvent
+                } }
             />
 
         </div>
